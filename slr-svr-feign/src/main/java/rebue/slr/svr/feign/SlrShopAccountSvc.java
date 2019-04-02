9195165170dacafa375793321054c1373ad51fda
lbl <lbl@lbl-PC>
2019-04-02 14:21:54 +0800

    新增店铺账号的内部调用方法

---
+++ slr-svr-feign/src/main/java/rebue/slr/svr/feign/SlrShopAccountSvc.java
@@ -0,0 +1,20 @@
+package rebue.slr.svr.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import rebue.sbs.feign.FeignConfig;
+import rebue.slr.mo.SlrShopAccountMo;
+
+@FeignClient(name = "slr-svr", configuration = FeignConfig.class)
+public interface SlrShopAccountSvc {
+
+	/**
+	 * 获取单个店铺账号
+	 *
+	 * @mbg.generated 自动生成，如需修改，请删除本行
+	 */
+	@GetMapping("/slr/shopaccount/getbyid")
+	SlrShopAccountMo getById(@RequestParam("id") java.lang.Long id);
+}
